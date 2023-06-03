51107fb0081f39092fc7a394e7e5eee1f0868e3d
lfowks <lfowks@gmail.com>
2023-06-03 15:56:44 -0600

    Debounce Time

--- src/components/DebounceTime.jsx
+++ src/components/DebounceTime.jsx
@@ -1,7 +1,26 @@
+
+
 const DebounceTime = () => {
 
-  const handleChange = (event) =>{
+  let debounceTime = null;
+
+  const callBackFunction = (value) => {
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  const handleChange = (event) => {
+    
     console.log(event.target.value);
+
+    clearTimeout(debounceTime);
+
+    debounceTime = setTimeout(() => {
+        callBackFunction(event.target.value)        
+    }, 500);   
+
   }
 
   return (
